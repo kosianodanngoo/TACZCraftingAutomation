3fa8ae36326ad73d93e98156f72bed077373805d
kosianodanngoo <kosian@odangoo.com>
2025-09-16 13:40:56 +0900

    コンフィグのコメントを修正

--- src/main/java/com/taczcraftingautomation/ModConfig.java
+++ src/main/java/com/taczcraftingautomation/ModConfig.java
@@ -5,8 +5,8 @@ import net.minecraftforge.common.ForgeConfigSpec;
 public class ModConfig {
     private static final ForgeConfigSpec.Builder BUILDER = new ForgeConfigSpec.Builder();
 
-    public static final ForgeConfigSpec.IntValue AUTOMATIC_SMITH_TABLE_ENERGY_CAPACITY = BUILDER.comment("A magic number").defineInRange("automaticSmithTableEnergyCapacity", 10000, 0, Integer.MAX_VALUE);
-    public static final ForgeConfigSpec.IntValue AUTOMATIC_SMITH_TABLE_ENERGY_USAGE = BUILDER.comment("A magic number").defineInRange("automaticSmithTableEnergyUsage", 100, 0, Integer.MAX_VALUE);
+    public static final ForgeConfigSpec.IntValue AUTOMATIC_SMITH_TABLE_ENERGY_CAPACITY = BUILDER.comment("Energy capacity of Automatic Smith Table").defineInRange("automaticSmithTableEnergyCapacity", 10000, 0, Integer.MAX_VALUE);
+    public static final ForgeConfigSpec.IntValue AUTOMATIC_SMITH_TABLE_ENERGY_USAGE = BUILDER.comment("Energy usage per tick of Automatic Smith Table").defineInRange("automaticSmithTableEnergyUsage", 100, 0, Integer.MAX_VALUE);
 
     static final ForgeConfigSpec SPEC = BUILDER.build();
 
